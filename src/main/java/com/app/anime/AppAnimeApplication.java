a01e79b4ff253e7f101800819fc70189e5e3f1a0
Yerson Saenz <saenzandres2018@gmail.com>
2025-07-25 11:19:18 -0500

    De RepositorioUsuario se elimina la funcion tablaUsuarioExiste y se actualiza la funcion crearTablaUsuario para que la consulta sea la que valida si existe o no la tabla, se pasa la conexion como parametro para que sea mas eficiente

--- src/main/java/com/app/anime/AppAnimeApplication.java
+++ src/main/java/com/app/anime/AppAnimeApplication.java
@@ -1,8 +1,14 @@
 package com.app.anime;
 
 
+import com.app.anime.model.RepositorioUsuario;
+import com.app.anime.model.Usuario;
+
 public class AppAnimeApplication {
     public static void main(String [] args) {
+
+        Usuario usuarioPrueba = new Usuario("Andres", "Yoforever*");
+        RepositorioUsuario.agregarNuevoUsuario(usuarioPrueba);
 
     }
 }
--- src/main/java/com/app/anime/model/RepositorioUsuario.java
+++ src/main/java/com/app/anime/model/RepositorioUsuario.java
@@ -10,56 +10,36 @@ public class RepositorioUsuario {
 
     //Metodo encargado de guardar un nuevo usuario si este no existe por correo
     public static void agregarNuevoUsuario(Usuario usuario){
-        String sql = "INSET INTO tbUsuarios (usuario, contraseña) VALUES (?,?)";
 
-        try(Connection conn = PostgresConector.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql)){
+        try(Connection conn = PostgresConector.getConnection()) {
 
-            crearTablaUsuarios();
-            stmt.setString(1, usuario.getUsuario());
-            stmt.setString(2, usuario.getContraseña());
+            crearTablaUsuarios(conn);
 
-            stmt.executeUpdate();
+            String sql = "INSERT INTO tbUsuarios (usuario, contraseña) VALUES (?,?)";
 
-            JOptionPane.showMessageDialog(null, "Registro exitoso.");
+            try( PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+                stmt.setString(1, usuario.getUsuario());
+                stmt.setString(2, usuario.getContraseña());
+                stmt.executeUpdate();
 
-    //Metodo encargado de comprobar si tbUsuarios existe en usuarios_db_app_animes
-    public static boolean tablaUsuariosExiste(){
-        String consulta = "SELECT EXISTS (" +
-                "SELECT FROM information_schema.tables " +
-                "WHERE table_schema = 'public' AND table_name = 'tbUsuarios'" +
-                ");";
-        try(Connection conn = PostgresConector.getConnection();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(consulta)
-            ) {
-            if(rs.next()){ return rs.getBoolean(1); }
-        } catch (Exception e) { e.printStackTrace(); }
-        return false;
+                JOptionPane.showMessageDialog(null, "Registro exitoso.");
+            }
+        } catch (Exception e) { throw new RuntimeException(e); }
     }
 
     //Metodo encargado de crear tbUsuarios si no existe
-    public static void crearTablaUsuarios(){
-        if(!tablaUsuariosExiste()){
-            String sql = "CREATE TABLE tbUsuarios (" +
-                    "id SERIAL PRIMARY KEY, " +
-                    "usuario VARCHAR(50) NOT NULL, " +
-                    "contraseña VARCHAR(100) NOT NULL" +
-                    ");";
+    public static void crearTablaUsuarios(Connection conn){
+        String sql = "CREATE TABLE IF NOT EXISTS tbUsuarios (" +
+                "id SERIAL PRIMARY KEY, " +
+                "usuario VARCHAR(50) NOT NULL, " +
+                "contraseña VARCHAR(100) NOT NULL" +
+                ");";
 
-            try (Connection conn = PostgresConector.getConnection();
-                Statement stmt = conn.createStatement();
-                ){
-
-                stmt.executeUpdate(sql); //Actualiza la base de datos con la nueva tabla
-            } catch (Exception e){
-                e.printStackTrace();
-            }
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(sql); //Actualiza la base de datos con la nueva tabla
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
